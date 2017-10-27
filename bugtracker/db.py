4e54f98759f8dce0a70b2ec88ee2a3c88eb6dc00
ofisser86 <ofisser86@gmail.com>
2017-10-27 11:57:19 +0800

    create bugtracker app. first commit

---
+++ bugtracker/db.py
@@ -0,0 +1,9 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'HOST': 'localhost',
+        'USER': 'bugtracker_db_user',
+        'PASSWORD': 'rh705743',
+        'NAME': 'bugtracker_db',
+    }
+}
