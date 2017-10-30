9e6fbe18c5620c199fefb914ac00942e32a6fc15
ofisser86 <ofisser86@gmail.com>
2017-10-30 11:15:25 +0800

    create templates of css, js, html, url, add static and media folder

--- bugtracker/settings.py
+++ bugtracker/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'x&2u88hzbzq*%id@1-h0uo&fxjkth!o=l^p^9*4t5owl=uv56!'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'issuetracker',
 ]
 
 MIDDLEWARE = [
@@ -111,3 +112,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
