a4021578261244f5d3b76b96a51593ae8b0f6c2a
ofisser86 <ofisser86@gmail.com>
2017-10-30 17:37:25 +0800

    add login redirect to home page

--- bugtracker/settings.py
+++ bugtracker/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 
 import os
 from .db import *
+from django.core.urlresolvers import reverse_lazy
+LOGIN_REDIRECT_URL = reverse_lazy('home')
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
